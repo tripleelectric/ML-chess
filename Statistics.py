286fe9616b6e3df8784084be21e4f17d7c78b593
tripleelectric <z.abacani@gmail.com>
2025-10-01 20:08:27 +0200

    implementing basic statistical functions somewhat from scratch, so its more interpretable than just calling np.mean or pandas.mean

---
+++ Statistics.py
@@ -0,0 +1,28 @@
+class Statistics:
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def mean(vector):
+        """
+        For intuition: mean sometimes is called the "expectation"
+        """
+        return sum(vector) / len(vector)
+
+    @staticmethod
+    def mode(vector):
+        if len(vector) % 2 == 0:
+            index_one = int(len(vector) / 2)
+            index_two = index_one - 1
+            return (vector[index_one] + vector[index_two]) / 2
+
+    @staticmethod
+    def variance(vector):
+        mean = Statistics.mean(vector)
+        n = len(vector)
+        return sum([vi - mean for vi in vector]) / n
+
+
+v = [1, 1, 2, 3, 4, 5]
+print(Statistics.mode(v))
