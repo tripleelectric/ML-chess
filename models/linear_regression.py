a3d4899dfd2da366d09ee0652156699e22494702
tripleelectric <z.abacani@gmail.com>
2025-10-09 13:29:41 +0200

    streamlining

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -2,10 +2,15 @@ import numpy as np
 import pandas as pd
 
 # Load the dataset
-df = pd.read_csv('../data/games.csv')
+df = pd.read_csv("../data/games.csv")
+
 
 class LinearRegression:
-    def __init__(self, learning_rate=0.01, epochs=300, ):
+    def __init__(
+        self,
+        learning_rate=0.01,
+        epochs=300,
+    ):
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.weights = None
@@ -21,8 +26,5 @@ class LinearRegression:
         """
         n_samples, n_features = X.shape()
 
-        self.weights = np.zeros(n_features) # for every feature 1 weight
+        self.weights = np.zeros(n_features)  # for every feature 1 weight
         self.bias = 0
-
-
-
